4352f0b19db3205fb57de6792b4a3d76a5f99f86
alec <kaltschmidalec@gmail.com>
2021-08-08 11:33:20 -0700

    commit tree cb01b66ea95c8f5b3669cea30698b48f8a553ef7

--- index.js
+++ index.js
@@ -1,4 +1,82 @@
 /* Your Code Here */
+function createEmployeeRecord(employeeInfo) {
+    let employee = {
+        firstName: employeeInfo[0],
+        familyName: employeeInfo[1],
+        title: employeeInfo[2],
+        payPerHour: employeeInfo[3],
+        timeInEvents: [],
+        timeOutEvents: []
+    }
+    return employee
+}
+
+function createEmployeeRecords(employees) {
+    return employees.map( employee => {
+        return createEmployeeRecord(employee)
+    })
+}
+
+let createTimeInEvent = function(dateStamp){
+    let [date, hour] = dateStamp.split(" ")
+    
+    this.timeInEvents.push({
+        type: "TimeIn", 
+        hour: parseInt(hour),
+        date: date
+    })
+    return this
+}
+
+let createTimeOutEvent = function(dateStamp){
+    let [date, hour] = dateStamp.split(" ")
+    
+    this.timeOutEvents.push({
+        type: "TimeOut", 
+        hour: parseInt(hour),
+        date: date
+    })
+    return this
+}
+
+let hoursWorkedOnDate = function(desiredDate){
+    let timeIn = this.timeInEvents.find( e => {
+        return e.date === desiredDate
+    })
+    let timeOut = this.timeOutEvents.find( e => {
+        return e.date === desiredDate
+    })
+    return (timeOut.hour - timeIn.hour)/100
+}
+
+let wagesEarnedOnDate = function(desiredDate){
+    let wages = hoursWorkedOnDate.call(this, desiredDate) * this.payPerHour
+    return wages
+}
+
+let findEmployeeByFirstName = function(srcArray, firstName){
+    return srcArray.find(e => {
+        return e.firstName = firstName
+    })
+}
+
+let calculatePayroll = function(employeeArray){
+    let totalPayroll = employeeArray.reduce( function(accumulator, employee){
+        return accumulator + allWagesFor.call(employee)
+    }, 0)
+    return totalPayroll
+} 
+
+
+
+
+
+
+
+
+
+
+
 
 /*
  We're giving you this function. Take a look at it, you might see some usage
